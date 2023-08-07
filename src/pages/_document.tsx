96cc952a6dc6f362cb9f95fbe8e9f91336b7ab94
Luiz Henrique <7henrique18@gmail.com>
2023-08-07 20:45:42 -0300

    fix: Setup initial SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="theme-color" content="#db2777" />
+      </Head>
 
       <body className="bg-zinc-950 text-zinc-500">
         <Main />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,39 @@
+import { NextSeo } from "next-seo";
 import { HomeTemplate } from "src/templates/Home";
 
+const thumbnail = {
+  url: "https://lui7henrique.com/about.png",
+  width: 1200,
+  height: 630,
+  alt: "About me",
+};
+
+const defaultMetadata = {
+  title: "Luiz Henrique • About me",
+  description:
+    "Hello, I'm Luiz Henrique! I'm a software engineer currently based in Brazil. 🖐",
+};
+
 export default function Home() {
-  return <HomeTemplate />;
+  console.log({ testENV: process.env });
+
+  return (
+    <>
+      <NextSeo
+        canonical={process.env.NEXT_PUBLIC_VERCEL_URL}
+        twitter={{
+          ...defaultMetadata,
+          cardType: "summary_large_image",
+          site: "@lui7henrique",
+        }}
+        openGraph={{
+          ...defaultMetadata,
+          url: process.env.NEXT_PUBLIC_VERCEL_URL,
+          images: [thumbnail],
+        }}
+      />
+
+      <HomeTemplate />
+    </>
+  );
 }
