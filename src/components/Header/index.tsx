885d86b5c959df94d4d47249258c60d5091a88e7
Luiz Henrique <7henrique18@gmail.com>
2023-08-07 18:19:23 -0300

    feat: Setup tooltip to show info in title

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,12 +4,14 @@ import Tooltip from "../Tooltip";
 
 export const Header = () => {
   return (
-    <header className="flex border-b border-zinc-900 pb-4 justify-between items-center">
-      <Link href="/">
-        <h1 className="font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-700">
-          Cine
-        </h1>
-      </Link>
+    <header className="flex border-b border-zinc-900 pb-4 justify-between items-start">
+      <Tooltip content="Movies and TV series recommendations in one click!">
+        <Link href="/">
+          <h1 className="font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-700">
+            Cine
+          </h1>
+        </Link>
+      </Tooltip>
 
       <div>
         <nav className="flex gap-2 divide-x divide-zinc-900">
